48c9e33c07b94661a77713cb7e9c3d9acef8e0fc
Quimey <quimey2000@gmail.com>
2025-12-04 16:52:11 -0300

    Fixed gradient bug in note selection menu

--- app/src/main/java/com/qui/noted/MainActivity.kt
+++ app/src/main/java/com/qui/noted/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.qui.noted
 
+import android.R.attr.endY
+import android.R.attr.startY
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -24,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -68,9 +72,9 @@ fun NoteGrid() {
 @Composable
 fun GridItem(item: Int) {
 
-    val colorStops = arrayOf(
-        0.01f to LightText,
-        0.13f to Color.Transparent,
+    val colors = listOf(
+        Color.Transparent,
+        Color.LightGray
     )
 
     Card(
@@ -79,28 +83,39 @@ fun GridItem(item: Int) {
             .height(200.dp),
         shape = RoundedCornerShape(18.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.LightGray)
-                .padding(10.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text (
-                text = "Note $item",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold
-            )
-            Spacer(Modifier.height(5.dp))
-            Text(
-                text = stringResource(R.string.note_sample),
-                color = LightText,
-                style = TextStyle(
-                    brush = Brush.verticalGradient(
-                        colorStops = colorStops
-                    )
+        Box {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Note $item",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
                 )
+                Spacer(Modifier.height(5.dp))
+                Text(
+                    text = stringResource(R.string.note_sample),
+                    color = LightText
+                )
+            }
+
+            // Gradient
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = colors,
+                            startY = 100f,
+                            endY = 500f
+                        )
+                    )
             )
         }
     }
+
 }
